2fa2f0bb72a22e353c33efa03392e8a029962358
al-jabir <aljabir098@gmail.com>
2024-03-10 12:47:24 +0600

    logic handle

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ app.get('/products', async (req, res) => {
     const price = req.query.price;
 
     let products;
-    if (price) {
-      products = await Product.find({ price: { $gt: price } });
+    if (true) {
+      products = await Product.find({ $or: [{ $gt: price }, { rating: { $gt: 4 } }] });
     } else {
       products = await Product.find();
     }
